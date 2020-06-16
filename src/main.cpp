8e05d2a0415c297fea427e84534caaf50cde5a75
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2020-06-16 10:13:17 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using std::cout;
+using std::endl;  
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << "\tfile.stl" << endl;
+    return EXIT_FAILURE;
+  }
+
+  std::string stlFile (argv[1]);
+  cout << stlFile << endl;
+
+
+  return EXIT_SUCCESS;
+}
